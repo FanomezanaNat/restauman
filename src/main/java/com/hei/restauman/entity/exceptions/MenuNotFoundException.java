eb786d2ed198cdface0b70bd4019290aed9b4ebc
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-05-27 21:55:59 +0300

    feat: impl MenuNotFoundException

---
+++ src/main/java/com/hei/restauman/entity/exceptions/MenuNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hei.restauman.entity.exceptions;
+
+public class MenuNotFoundException extends RuntimeException{
+    public MenuNotFoundException(String message){
+        super(message);
+    }
+}
